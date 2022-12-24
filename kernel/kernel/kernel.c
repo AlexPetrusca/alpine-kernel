60eaf202dc4e3e0c71b01d584569b6eebe9c3bcc
petrusca <petruscad@yahoo.com>
2022-12-24 06:47:50 +0000

    Switch to Multiboot 2

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,204 +1,121 @@
 #include <stdio.h>
 #include <string.h>
-
-#include <sys/circ_buf.h>
-#include <kernel/kb.h>
 #include <kernel/tty.h>
 #include <kernel/sh.h>
 #include <kernel/vga.h>
 #include <kernel/cpu.h>
 #include <stdio_tests.h>
+#include <kernel/multiboot2.h>
 
-void test_circ_buf() {
-  char buffer[4096 * 4];
-  struct circ_buf cb_struct;
-  circ_buf_t cb = &cb_struct;
-  cb->buf = buffer;
-  cb->capacity = 4;
-  cb->granularity = 4096;
+char* mem_type[] = {"Available", "Reserved", "ACPI Reclaimable", "NVS", "Bad"};
 
-  char* s = "abc";
-  circ_buf_push(cb, s);
-  s = "def";
-  circ_buf_push(cb, s);
-  s = "ghi";
-  circ_buf_push(cb, s);
-  s = "jkl";
-  circ_buf_push(cb, s);
-  s = "mno";
-  circ_buf_push(cb, s);
-  s = "pqr";
-  circ_buf_push(cb, s);
+unsigned long kernel_addr;
 
-  printf("buffer (peekn_head): \n");
-  for (size_t i = 0; i < cb->size; i++) {
-    char* s = circ_buf_peekn_head(cb, i);
-    printf("    %d: %s\n", i, s);
-  }
-  printf("buffer (peekn_tail): \n");
-  for (size_t i = 0; i < cb->size; i++) {
-    char* s = circ_buf_peekn_tail(cb, i);
-    printf("    %d: %s\n", i, s);
-  }
-  printf("buffer (pop): \n");
-  for (int i = 0; !circ_buf_empty(cb); i++) {
-    char* s = circ_buf_pop(cb);
-    printf("    %d: %s\n", i, s);
-  }
-  printf("head: %d\n", cb->head);
-  printf("tail: %d\n", cb->tail);
-  printf("size: %d\n", cb->size);
-  printf("capacity: %d\n", cb->capacity);
-}
+void mbi() {
+  printf("Announced mbi size %d bytes\n", *(unsigned*) kernel_addr);
 
-void test_memchr() {
-  char* s1 = "";
-  char* s2 = "abcdefabcdef";
-  char* s3 = "11111111111111111111";
+  struct multiboot_tag* tag;
 
-  printf("Testing memchr():\nTest1...");
-  if (memchr(s1, 'x', 0) == NULL)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest2...");
-  if (memchr(s2, 'y', 0) == NULL)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest3...");
-  if ((char*) memchr(s2, 'a', 1) - s2 == 0)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest4...");
-  if (memchr(s2, 'd', 2) == NULL)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest5...");
-  if ((char*) memchr(s2, 'd', 12) - s2 == 3)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest6...");
-  if ((char*) memchr(s2, 'f', 12) - s2 == 5)
-    printf("passed.");
-  else printf("FAILED.");
-  printf("\nTest7...");
-  if ((char*) memchr(s3, '1', 20) - s3 == 0)
-    printf("passed.");
-  else printf("FAILED.");
-  putchar('\n');
-}
+  for (tag = (struct multiboot_tag*) (kernel_addr + 8);
+       tag->type != MULTIBOOT_TAG_TYPE_END;
+       tag = (struct multiboot_tag*) ((multiboot_uint8_t*) tag + ((tag->size + 7) & ~7))) {
+    printf("Tag 0x%x, Size %d: ", tag->type, tag->size);
+    switch (tag->type) {
+      case MULTIBOOT_TAG_TYPE_CMDLINE:
+        printf("Command line = %s\n", ((struct multiboot_tag_string*) tag)->string);
+        break;
 
-void test_memrchr() {
-  printf("Testing memrchr():\n");
-  const char str[] = "http://www.tutorialspoint.com";
-  const char ch = '.';
+      case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
+        printf("Boot loader name = %s\n", ((struct multiboot_tag_string*) tag)->string);
+        break;
 
-  char* ret = memrchr(str, ch, strlen(str));
-  printf("String after |%c| is - |%s|\n", ch, ret);
-}
+      case MULTIBOOT_TAG_TYPE_MODULE:
+        printf("Module at 0x%x-0x%x. Command line %s\n",
+               ((struct multiboot_tag_module*) tag)->mod_start,
+               ((struct multiboot_tag_module*) tag)->mod_end,
+               ((struct multiboot_tag_module*) tag)->cmdline);
+        break;
 
-void test_strchr() {
-  printf("Testing strchr():\n");
-  const char str[] = "http://www.tutorialspoint.com";
-  const char ch = '.';
+      case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
+        printf("mem_lower = %uKB, mem_upper = %uKB\n",
+               ((struct multiboot_tag_basic_meminfo*) tag)->mem_lower,
+               ((struct multiboot_tag_basic_meminfo*) tag)->mem_upper);
+        break;
 
-  char* ret = strchr(str, ch);
-  printf("String after |%c| is - |%s|\n", ch, ret);
-}
+      case MULTIBOOT_TAG_TYPE_BOOTDEV:
+        printf("Boot device 0x%x,%u,%u\n",
+               ((struct multiboot_tag_bootdev*) tag)->biosdev,
+               ((struct multiboot_tag_bootdev*) tag)->slice,
+               ((struct multiboot_tag_bootdev*) tag)->part);
+        break;
 
-void test_strrchr() {
-  printf("Testing strrchr():\n");
-  const char str[] = "http://www.tutorialspoint.com";
-  const char ch = '.';
+      case MULTIBOOT_TAG_TYPE_MMAP: {
+        printf("mmap\n");
+        multiboot_memory_map_t* mmap;
+        for (mmap = ((struct multiboot_tag_mmap*) tag)->entries;
+             (multiboot_uint8_t*) mmap < (multiboot_uint8_t*) tag + tag->size;
+             mmap = (multiboot_memory_map_t*)
+               ((unsigned long) mmap
+                 + ((struct multiboot_tag_mmap*) tag)->entry_size))
+          printf(" base_addr = 0x%x%x, length = 0x%x%x, type = %s\n",
+                 (unsigned) (mmap->addr >> 32), (unsigned) (mmap->addr & 0xffffffff),
+                 (unsigned) (mmap->len >> 32), (unsigned) (mmap->len & 0xffffffff),
+                 mem_type[(unsigned) mmap->type]);
+        break;
+      }
 
-  char* ret = strrchr(str, ch);
-  printf("String after |%c| is - |%s|\n", ch, ret);
-}
+      case MULTIBOOT_TAG_TYPE_FRAMEBUFFER: {
+        struct multiboot_tag_framebuffer* tagfb = (struct multiboot_tag_framebuffer*) tag;
+        printf("screen %d x %d\n", tagfb->common.framebuffer_width, tagfb->common.framebuffer_height);
+        break;
+      }
 
-void test_strtok() {
-  printf("Testing strtok():\n");
-  char str[80] = "This is - www.tutorialspoint.com - website";
-  const char s[2] = "-";
-  char* token;
+      case MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR: {
+        struct multiboot_tag_load_base_addr* base_addr = (struct multiboot_tag_load_base_addr*) tag;
+        printf("load base addr 0x%x\n", base_addr->load_base_addr);
+        break;
+      }
 
-  token = strtok(str, s);
-  while (token != NULL) {
-    printf("%s\n", token);
-    token = strtok(NULL, s);
-  }
-}
+      case MULTIBOOT_TAG_TYPE_APM: {
+        struct multiboot_tag_apm* apm = (struct multiboot_tag_apm*) tag;
+        printf("APM cseg=%d:%d, dseg=%d:%d\n", apm->cseg, apm->cseg_len, apm->dseg, apm->dseg_len);
+        break;
+      }
 
-void test_strtok_r() {
-  printf("Testing strtok_r():\n");
-  char str[80] = "This is - www.tutorialspoint.com - website";
-  const char s[2] = "-";
-  char* rest;
-  char* token;
+      case MULTIBOOT_TAG_TYPE_ELF_SECTIONS: {
+        struct multiboot_tag_elf_sections* elf = (struct multiboot_tag_elf_sections*) tag;
+        printf("%d ELF sections\n", elf->num);
+        break;
+      }
 
-  token = strtok_r(str, s, &rest);
-  while (token != NULL) {
-    printf("%s\n", token);
-    token = strtok_r(NULL, s, &rest);
-  }
-}
+      case MULTIBOOT_TAG_TYPE_ACPI_OLD: {
+        struct multiboot_tag_old_acpi* acpi = (struct multiboot_tag_old_acpi*) tag;
+        printf("RSDP table, %d bytes\n", acpi->size - 8);
+        break;
+      }
 
-void test_strstr() {
-  printf("Testing strstr():\n");
-  const char haystack[20] = "TutorialsPoint";
-  const char needle[10] = "Point";
-  char* ret;
-
-  ret = strstr(haystack, needle);
-  printf("The substring is: %s\n", ret);
-}
-
-void test_strcat() {
-  printf("Testing strcat():\n");
-  char src[50], dest[50];
-  strcpy(src, "This is source");
-  strcpy(dest, "This is destination");
-
-  strcat(dest, src);
-  printf("Final destination string : |%s|\n", dest);
-}
-
-void test_strncat() {
-  printf("Testing strncat():\n");
-  char src[50], dest[50];
-  strcpy(src, "This is source");
-  strcpy(dest, "This is destination");
-
-  strncat(dest, src, 15);
-  printf("Final destination string : |%s|\n", dest);
-}
-
-void test_kb() {
-  while (1) {
-    int scan = keyboard_readscan();
-    char ch = keyboard_scan2ch(scan);
-    if (keyboard_scan2release(scan)) {
-      keyboard_set_released(ch);
-    } else if (keyboard_is_released(ch)) {
-      keyboard_set_pressed(ch);
-      printf("{scan: %d, ch: %d, display: %c}\n", scan & 0x7F, ch, ch);
+      default:
+        printf("Unknown\n");
     }
   }
+  tag = (struct multiboot_tag*) ((multiboot_uint8_t*) tag + ((tag->size + 7) & ~7));
+  printf("Total mbi size %d bytes\n", (unsigned) tag - kernel_addr);
 }
 
-void kernel_main() {
+void cmain(unsigned long magic, unsigned long _kernel_addr) {
+  kernel_addr = _kernel_addr;
   terminal_initialize(&vga_tty_device);
-  sh_command commands[] = {{"cpu", print_cpu_info}, {"test", printf_tests}, {"", NULL}};
-  shell_start(commands);
-  // test(); // todo: remove me
 
-  // test_kb();
-  // test_circ_buf();
-  // test_memchr();
-  // test_memrchr();
-  // test_strchr();
-  // test_strrchr();
-  // test_strtok();
-  // test_strtok_r();
-  // test_strstr();
-  // test_strcat();
-  // test_strncat();
+  if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+    printf("Invalid magic number: 0x%x\n", (unsigned) magic);
+    return;
+  }
+
+  if (kernel_addr & 7) {
+    printf("Unaligned mbi: 0x%x\n", kernel_addr);
+    return;
+  }
+
+  sh_command commands[] = {{"cpu", print_cpu_info}, {"test", printf_tests}, {"mbi", mbi}, {"", NULL}};
+  shell_start(commands);
 }
