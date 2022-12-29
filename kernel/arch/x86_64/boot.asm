a28b2532e1287c1badb8beb84cfea262729b87e2
apetrusca <apetrusca@ebay.com>
2022-12-28 16:24:58 -0800

    refactor & clean up boot.asm

--- kernel/arch/x86_64/boot.asm
+++ kernel/arch/x86_64/boot.asm
@@ -4,13 +4,9 @@ ARCH        equ 0
 HEADER_SIZE equ header_end - header_start
 CHECKSUM    equ -(MAGIC + ARCH + HEADER_SIZE)
 
-; Page access bits
-PAGE_PRESENT    equ (1 << 0)
-PAGE_WRITE      equ (1 << 1)
-
-; GDT segment constants
-CODE_SEG    equ 0x0008
-DATA_SEG    equ 0x0010
+; CR0 bits
+CR0_PG   equ 1 << 31
+CR0_PE   equ 1 << 0
 
 ; CR4 bits
 CR4_PAE         equ 1 << 5
@@ -19,35 +15,43 @@ CR4_PGE         equ 1 << 7
 CR4_OSFXSR      equ 1 << 9
 CR4_OSXMMEXCPT  equ 1 << 10
 
-; CR0 bits
-CR0_PG equ 1 << 31
-CR0_PE equ 1 << 0
-
+; MSR bits
 IA32_EFER_MSR      equ  0xc0000080
 IA32_EFER_MSR_LME  equ  1 << 8
 
-PAGE_SIZE  equ  4096
+; GDT segment selectors
+CODE_SEG    equ 0x0008
+DATA_SEG    equ 0x0010
 
 ; GDT access bits
-PRESENT        equ 1 << 7
-NOT_SYS        equ 1 << 4
-EXEC           equ 1 << 3
-DC             equ 1 << 2
-RW             equ 1 << 1
 ACCESSED       equ 1 << 0
+RW             equ 1 << 1
+DC             equ 1 << 2
+EXEC           equ 1 << 3
+NOT_SYS        equ 1 << 4
+RING1          equ 1 << 5
+RING2          equ 1 << 6
+RING3          equ (1 << 5) | (1 << 6)
+PRESENT        equ 1 << 7
 
 ; GDT flag bits
-GRAN_4K       equ 1 << 7
-SZ_32         equ 1 << 6
 LONG_MODE     equ 1 << 5
+SZ_32         equ 1 << 6
+GRAN_4K       equ 1 << 7
+
+; Paging constants
+PAGE_SIZE    equ 4096   ; 4 KB
+PML4T_START  equ 0x1000
+PDPT_START   equ 0x2000
+PDT_START    equ 0x3000
+PT_START     equ 0x4000
+
+; Page access bits
+PAGE_PRESENT    equ (1 << 0)
+PAGE_WRITE      equ (1 << 1)
 
 ; Stack constants
-STACK_SIZE  equ 0x4000
-
-PML4T_START equ 0x1000
-PDPT_START equ 0x2000
-PDT_START equ 0x3000
-PT_START equ 0x4000
+STACK_SIZE   equ 16384   ; 16 KB
 
 section .multiboot
 
@@ -97,7 +101,6 @@ GDT:
         db 0                                        ; Base (mid, bits 16-23)
         db PRESENT | NOT_SYS | EXEC | RW            ; Access
         db GRAN_4K | LONG_MODE | 0xF                ; Flags & Limit (high, bits 16-19) (64-bit long mode)
-;        db GRAN_4K | SZ_32 | 0xF                    ; Flags & Limit (high, bits 16-19) (32-bit compatibility mode)
         db 0                                        ; Base (high, bits 24-31)
     .Data:
         dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
@@ -115,12 +118,12 @@ global _start:function (_end - _start)
 _start:
     mov esp, stack_top
 
-    push 0    ; Reset EFLAGS.
+    push 0  ; Reset EFLAGS.
     popf
 
-    push 0 ; Push the 64-bit pointer to the Multiboot information structure.
+    push 0  ; Push the 64-bit pointer to the Multiboot information structure.
     push ebx
-    push 0 ; Push the 64-bit magic value.
+    push 0  ; Push the 64-bit magic value.
     push eax
 
     ; Switch to long mode
