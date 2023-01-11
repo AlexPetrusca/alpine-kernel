6ffbdebfa4bdbb52261129ab5825384681bd516f
petrusca <petruscad@yahoo.com>
2023-01-11 02:57:04 +0000

    Various improvements.

--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -13,4 +13,15 @@
 #define __END_DECLS
 #endif
 
+#define static_assert _Static_assert
+
+// Allows getting the size of a type at compile time.
+//
+// Example of code:
+//    char (*__kaboom)[sizeof( long long )] = 1;
+// GCC error:
+//    error: invalid conversion from 'int' to 'char (*)[8]' [-fpermissive]
+// So sizeof(long long) == 8
+#define  DEBUG_SIZEOF(type)  uint8_t (*__kaboom)[sizeof( type )] = 1;
+
 #endif
