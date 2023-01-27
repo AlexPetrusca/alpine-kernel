2dec00105a4e08b87fa715c1ed59d06035d7ef4d
petrusca <petruscad@yahoo.com>
2023-01-27 17:44:30 +0000

    Make the build great again!

--- iso.sh
+++ iso.sh
@@ -8,6 +8,7 @@ mkdir -p build/isodir/boot/grub
 
 cp build/sysroot/boot/alpine.kernel build/isodir/boot/alpine.kernel
 cat > build/isodir/boot/grub/grub.cfg << EOF
+set timeout=0
 menuentry "Alpine Kernel" {
 	multiboot2 /boot/alpine.kernel
 }
