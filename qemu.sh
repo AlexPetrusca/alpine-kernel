2ae5f3579073be9e430616e84f876b768952f89a
dumitru-petrusca <120871405+dumitru-petrusca@users.noreply.github.com>
2023-01-26 22:56:27 -0800

    Reduce kernel size, add debug mode. (#37)

--- qemu.sh
+++ qemu.sh
@@ -2,11 +2,19 @@
 set -e
 . ./iso.sh
 
-echo "Running QEMU..."
+if [ "$1" = '-d' ]; then
+  x86_64-elf-objdump -M intel -D build/alpine.kernel > kernel.dump
+  debug=" -D qemu.log -d int"
+  echo "Debugging QEMU..."
+else
+  debug=""
+  echo "Running QEMU..."
+fi
+
 qemu-system-x86_64                                    \
 		-machine q35                                      \
 		-smp 1                                            \
 		-m 256M                                           \
 		-usb -device qemu-xhci,id=xhci -device usb-mouse  \
     -cdrom build/alpine.iso                           \
-    $@
+    $debug
